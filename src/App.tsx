164956f6d4ca13037310c1fd520072ae200bcb14
ku-nagata <ku-nagata@kddi.com>
2022-12-20 09:56:36 +0900

    ミニマム機能実装完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { ArrowForwardIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Textarea,
-  VStack,
-  Text,
-  Button,
-  HStack,
-  Icon,
-} from "@chakra-ui/react";
-import React from "react";
+import { Box, Textarea, VStack, Text, Button, HStack } from "@chakra-ui/react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [participants, setParticipants] = useState<Array<string>>([]);
+  const [shuffled, setShuffled] = useState<Array<string>>([]);
+
+  function shuffle(array: Array<string>) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
   return (
     <Box w="100vw" h="100vh" px="20px" pb="50px">
       <VStack>
@@ -20,9 +23,23 @@ function App() {
         </Text>
         <VStack align="left" w="100%" maxWidth="300px">
           <Text align="left">参加者</Text>
-          <Textarea h="110px" placeholder={`太郎\n次郎\n花子\n...`} />
+          <Textarea
+            h="110px"
+            placeholder={`太郎\n次郎\n花子\n...`}
+            onChange={(text) => {
+              const rows = text.target.value.split(/\n/);
+              setParticipants(rows.filter((row) => row !== ""));
+            }}
+          />
         </VStack>
-        <Button>シャッフル</Button>
+        <Button
+          onClick={() => {
+            const tmp = participants.slice();
+            setShuffled(shuffle(tmp));
+          }}
+        >
+          シャッフル
+        </Button>
       </VStack>
       <Box h="50px" />
       <VStack>
@@ -30,11 +47,22 @@ function App() {
           結果
         </Text>
         <VStack>
-          {/* TODO: map */}
           <HStack>
-            <Text>太郎</Text>
-            <ArrowForwardIcon />
-            <Text>花子</Text>
+            <VStack>
+              {participants.map((participant, index) => {
+                return (
+                  <HStack key={index}>
+                    <Text>{participant}</Text>
+                    <ArrowForwardIcon />
+                  </HStack>
+                );
+              })}
+            </VStack>
+            <VStack>
+              {shuffled.map((result, index) => {
+                return <Text key={index}>{result}</Text>;
+              })}
+            </VStack>
           </HStack>
         </VStack>
       </VStack>
